d0ee3653c0ab6967fb4a75b82c23d1cbe19c8a61
Abhijit105 <abhijitkumar10.5@gmail.com>
2024-08-16 19:19:43 +0530

    add updateBlog functionality

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -3,13 +3,16 @@ const {
   createBlog,
   getBlogById,
   getBlogs,
+  updateBlogById,
 } = require('../controller/BlogController')
 const { isLogged } = require('../middleware/authentication')
+const { isAuthorized } = require('../middleware/authorization')
 
 const router = express.Router()
 
 router.post('/', isLogged, createBlog)
 router.get('/:id', getBlogById)
 router.get('/', getBlogs)
+router.patch('/:id', isAuthorized, updateBlogById)
 
 module.exports = router
